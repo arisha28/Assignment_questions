27d9447341a31b267b672798699a048709f3001e
Arisha Ali <167895508+arisha28@users.noreply.github.com>
2025-09-10 11:33:23 +0530

    Delete star17.java

--- star17.java
+++
@@ -1,9 +0,0 @@
-public class star17 {
-    public static void main(String[] args) {
-         for (int row = 0; row < 10; row++) {
-             for (int i = 0; i < row; i++) {
-                 System.out.print(i);
-             }System.out.println("");
-         }
-    }
-}
